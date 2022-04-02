7407e522a37d459cf7ad602cdb0bac222d49274b
MarcoPelzer <marco.pelzer99@web.de>
2022-04-02 14:58:44 +0200

    Form Template Method

--- wwi20b2-workshop1-master/src/main/java/dhbw/fowler1/videostore/Customer.java
+++ wwi20b2-workshop1-master/src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -12,22 +12,48 @@ _name = name;
 }
 
 public String statement() {
-    Enumeration rentals = _rentals.elements();
-    String result = "Rental Record for " + getName() + "\n";
-    while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(each.getCharge()) + "\n";
-    }
-    result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-    result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-        " frequent renter points";
+    return new TextStatement().value(this);
+}
+
+class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerString(aCustomer);
+            while (rentals.hasMoreElements()) {
+                Rental each = (Rental) rentals.nextElement();
+                result += eachRentalString(each);
+             }
+        result += footerString(aCustomer);
     return result;
+            }
+    String eachRentalString (Rental aRental) {
+        return "\t" + aRental.getMovie().getTitle()+ "\t" +
+        String.valueOf(aRental.getCharge()) + "\n"; 
+        }
+        String footerString (Customer aCustomer) { 
+            return "Amount owed is " +
+        String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+        "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
+        }
+    String headerString(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() + "\n";
+    }
+}
+
+abstract class TextStatement {
+    abstract String headerString(Customer aCustomer); 
+    abstract String eachRentalString (Rental aRental); 
+    abstract String footerString (Customer aCustomer);
 }
 
 
 public String htmlStatement() {
-    Enumeration rentals = _rentals.elements();
+    return new HtmlStatement().value(this);
+}
+
+class HtmlStatement {
+    public String value(Customer aCostumer){
+        Enumeration rentals = _rentals.elements();
     String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n"; 
     while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement(); //show figures for each rental
@@ -40,7 +66,12 @@ public String htmlStatement() {
     String.valueOf(getTotalFrequentRenterPoints()) +
     "</EM> frequent renter points<P>"; 
     return result;
+    }
+    String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
 }
+
 
 public void addRental(Rental arg) {
 _rentals.addElement(arg);
