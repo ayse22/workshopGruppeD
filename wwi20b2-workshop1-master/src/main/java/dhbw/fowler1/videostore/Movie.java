8e82afbfd3b01aa2f8ccc96127c081e0f7c3e31d
MarcoPelzer <marco.pelzer99@web.de>
2022-04-02 14:01:18 +0200

    Replace Type Code with State/Strategy

--- wwi20b2-workshop1-master/src/main/java/dhbw/fowler1/videostore/Movie.java
+++ wwi20b2-workshop1-master/src/main/java/dhbw/fowler1/videostore/Movie.java
@@ -14,14 +14,49 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
-    public void setPriceCode(int _priceCode) {
-        this._priceCode = _priceCode;
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+            break;
+        default:
+            throw new
+    IllegalArgumentException
+            ("Incorrect Price Code");
+        }
     }
+    
+private Price _price;
 
     public String getTitle() {
         return _title;
+    }
+
+    double getCharge(int daysRented){
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR: result += 2;
+            if (getDaysRented() > 2)
+            result += (getDaysRented() - 2) * 1.5;
+            break;
+            case Movie.NEW_RELEASE:
+            result += getDaysRented() * 3;
+            break;
+            case Movie.CHILDRENS:
+            result += 1.5;
+            if (getDaysRented() > 3)
+            result += (getDaysRented() - 3) * 1.5;
+            break;    
+        }
+        return result;
     }
 }
---
+++ wwi20b2-workshop1-master/src/main/java/dhbw/fowler1/videostore/Price.java
@@ -0,0 +1,20 @@
+package main.java.dhbw.fowler1.videostore;
+
+abstract class Price{
+    abstract int getPriceCode();
+}
+class ChildrensPrice extends Price {
+    int getPriceCode() { 
+        return Movie.CHILDRENS;
+    }
+}
+class NewReleasePrice extends Price { 
+    int getPriceCode() {
+        return Movie.NEW_RELEASE; 
+    }
+}
+class RegularPrice extends Price {
+    int getPriceCode() { 
+        return Movie.REGULAR;
+    }
+}
